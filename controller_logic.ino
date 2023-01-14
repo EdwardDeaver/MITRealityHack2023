b3d2446513311daf9dee7bd4aa185ac9fba21d08
goyay790 <goyay790@newschool.edu>
2023-01-14 10:59:24 -0500

    Create controller_logic.ino

---
+++ controller_logic.ino
@@ -0,0 +1,55 @@
+//PINOUT
+
+//Potentiometer Dials
+
+//Tone Generator 
+#DEFINE POT_1 = 13; //Frequency 
+#DEFINE POT_2 = 12; //Wavelength
+#DEFINE POT_3 = 14; //Amplitude
+#DEFINE POT_4 = 27; //Speed
+
+//Mixer Knobs
+#DEFINE POT_5 = ; //Tone Gen
+#DEFINE POT_6 = ; //Ultrasonic
+
+//Contextal Knob
+#DEFINE POT_7 = ;
+
+//Toggle Switchs
+#DEFINE TOGGLE_1 =; //Tone Gen
+#DEFINE TOGGLE_2 =; //Ultrasonic
+
+//Pressure Pads
+#DEFINE FINGERPAD_1 = 33;
+#DEFINE FINGERPAD_2 = 32;
+#DEFINE FINGERPAD_3 = 35;
+#DEFINE FINGERPAD_4 = 34;
+
+//ROTARY ENCODER
+#DEFINE ROTARY_CLK =:
+#DEFINE ROTARY_DT =;
+#DEFINE ROTARY_SW =;
+
+//JOYSTICK
+#DEFINE JOYSTICK_VRX =;
+#DEFINE JOYSTICK_VRY  =;
+#DEFINE JOYSTICK_SW  =;
+
+//ULTRASONIC
+#DEFINE JULTRASONIC_TRIG = 25;
+#DEFINE JULTRASONIC_ECHO = 26;
+
+//LED
+#DEFINE LED_1 =; //Main
+#DEFINE LED_2 =; //Tone
+#DEFINE LED_3 =; //Ultra
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
